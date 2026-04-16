bef182619dfa2c27f12f5236d0cbdff8146f3680
Ambarwati Royani <ambarwatiroyanii@gmail.com>
2026-04-16 23:16:08 +0700

    mengupdate projek

--- lib/main_page.dart
+++ lib/main_page.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:pratikum_limit_kuota_kelompok6/src/features/home_page.dart';
-import 'package:pratikum_limit_kuota_kelompok6/src/features/settings/settings_page.dart';
+import 'package:pratikum_limit_kuota_kelompok6/settings_page.dart';
 
 class MainPage extends StatefulWidget {
   const MainPage({super.key});
@@ -18,7 +17,7 @@ class _MainPageState extends State<MainPage> {
   void initState() {
     super.initState();
 
-    _pages = [
+    _pages = const [
       HomePage(),
       SettingsPage(),
     ];
